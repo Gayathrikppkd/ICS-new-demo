584823fbd3449f458a5a82efdb9167bb3fb079cc
gayathri <gayathrikpgayathri16@gmail.com>
2024-05-21 14:27:50 +0530

    It'demo work

---
+++ script.js
@@ -0,0 +1,59 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation1: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+    },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation2: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+  });
+  
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation2: {
+      nextEl: ".swiper-button-next3",
+      prevEl: ".swiper-button-prev3",
+    },
+  });
+  var swiper4 = new Swiper(".mySwiper4", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation2: {
+      nextEl: ".swiper-button-next4",
+      prevEl: ".swiper-button-prev4",
+    },
+  });
